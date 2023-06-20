9f3dfadb1a45336cfd277bb199f70e8964c497b3
Amine <mohammed-el-amine.djellal@epitech.eu>
2023-06-20 16:39:37 +0200

    [ADD] create signature for admin good too

--- stock/src/Controller/AdminController.php
+++ stock/src/Controller/AdminController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -21,14 +20,17 @@ use Doctrine\ORM\EntityManagerInterface;
 use Mailgun\Mailgun;
 use Symfony\Component\Uid\Uuid;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use App\Entity\Signature;
 use DateTime;
+use DateTimeImmutable;
 use App\Entity\Logo;
 
 class AdminController extends AbstractController
@@ -264,10 +266,6 @@ class AdminController extends AbstractController
             throw $this->createNotFoundException('Utilisateur non trouvé');
         }
 
-        // if (!$user->isTokenValid()) {
-        //     return $this->redirectToRoute('app_home');;
-        // }
-
         $form = $this->createFormBuilder($user)
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
@@ -396,7 +394,7 @@ class AdminController extends AbstractController
         $logo = new Logo();
         $logo->setCreateAt($currentDateTime);
         $logo->setUpdateAt($currentDateTime);
-        
+
         $form = $this->createFormBuilder($logo)
             ->add('name', TextType::class, [
                 'label' => 'Nom :',
@@ -416,31 +414,204 @@ class AdminController extends AbstractController
             ])
             ->getForm();
 
-            $form->handleRequest($request);
-    
-            if ($form->isSubmitted() && $form->isValid()) {
-                $entityManager->persist($logo);
-                $entityManager->flush();
-                
-                $this->addFlash('success', 'Le logo a été ajouté avec succès.');
-                return $this->redirectToRoute('admin_logo');
-            }
+        $form->handleRequest($request);
 
-            $logoRepository = $entityManager->getRepository(Logo::class);
-            $logos = $logoRepository->findAll();
-            
-            return $this->render('admin/logo.html.twig', [
-                'form' => $form->createView(),
-                'logos' => $logos,
-            ]);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($logo);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Le logo a été ajouté avec succès.');
+            return $this->redirectToRoute('admin_logo');
+        }
+
+        $logoRepository = $entityManager->getRepository(Logo::class);
+        $logos = $logoRepository->findAll();
+
+        return $this->render('admin/logo.html.twig', [
+            'form' => $form->createView(),
+            'logos' => $logos,
+        ]);
     }
 
     #[Route('/admin/create-signature', name: 'admin_create_signature', methods: ['GET', 'POST'])]
     /**
      * @Route("/admin/create-signature", name="admin_create_signature", methods={"GET","POST"})
      */
-    public function createSignature(Request $request, EntityManagerInterface $entityManager, SessionInterface $session, UserRepository $userRepository, UrlGeneratorInterface $urlGenerator)
+    public function createSignature(Request $request, EntityManagerInterface $entityManager, SessionInterface $session, UserRepository $userRepository, UrlGeneratorInterface $urlGenerator, LogoRepository $logoRepository,)
     {
-        
+        if (!$session->has('user_id')) {
+            return new RedirectResponse($urlGenerator->generate('app_home'));
+        }
+        $userId = $session->get('user_id');
+        if ($userId) {
+            $currentUser = $userRepository->find($userId);
+            if ($currentUser && $currentUser->getRole() !== 'admin') {
+                throw $this->createAccessDeniedException('Access Denied');
+            }
+        } else {
+            throw $this->createAccessDeniedException('Access Denied');
+        }
+
+        $generatedSignature = '';
+        $logos = $logoRepository->findAll();
+        $logoChoices = [];
+
+        foreach ($logos as $logo) {
+            $name = $logo->getName();
+            $path = $logo->getPath();
+            $logoChoices[$name] = $logo;
+        }
+
+        $form = $this->createFormBuilder()
+            ->add('name', TextType::class, [
+                'label' => 'Nom et Prénom : ',
+                'attr' => [
+                    'placeholder' => 'Nom et Prénom',
+                ],
+            ])
+            ->add('role', TextType::class, [
+                'label' => 'Rôle : ',
+                'attr' => [
+                    'placeholder' => 'Poste dans l\'entreprise',
+                ],
+            ])
+            ->add('organization', TextType::class, [
+                'label' => 'Organisation : ',
+                'attr' => [
+                    'placeholder' => 'Nom de l\'entreprise'
+                ],
+            ])
+            ->add('adress', TextType::class, [
+                'label' => 'Adresse : ',
+                'attr' => [
+                    'placeholder' => 'Addresse',
+                ],
+            ])
+            ->add('zip_code', TextType::class, [
+                'label' => 'Code postal : ',
+                'attr' => [
+                    'placeholder' => 'Code Postal',
+                ],
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Ville : ',
+                'attr' => [
+                    'placeholder' => 'Ville',
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email : ',
+                'attr' => [
+                    'placeholder' => 'Email',
+                ],
+            ])
+            ->add('phone', TelType::class, [
+                'label' => 'Téléphone : ',
+                'attr' => [
+                    'placeholder' => 'Tél',
+                ],
+            ])
+            ->add('logo', EntityType::class, [
+                'label' => 'Logo : ',
+                'class' => Logo::class,
+                'choice_label' => 'name',
+                'choice_attr' => function ($key) {
+                    return ['name' => $key->getPath()];
+                },
+                'choices' => $logoChoices,
+                'required' => false,
+            ])
+            ->add('signatureSubmit', SubmitType::class, [
+                'label' => 'Générer la signature',
+                'attr' => [
+                    'class' => 'btn btn-primary',
+                    'name' => 'signatureSubmit', // Ajout de l'attribut name
+                ],
+            ])
+            ->getForm();
+
+
+        if ($request->isMethod('POST')) {
+            $data = $request->request->all();
+
+            if (isset($data['form']['signatureSubmit'])) {
+                $form->handleRequest($request);
+
+                if ($form->isSubmitted() && $form->isValid()) {
+                    $data = $form->getData();
+                    // Créer une instance de l'entité Signature et définir les valeurs des propriétés
+                    $signature = new Signature();
+                    $signature->setName($data['name']);
+                    $signature->setRole($data['role']);
+                    $signature->setOrganization($data['organization']);
+                    $signature->setAdress($data['adress']);
+                    $signature->setZipCode($data['zip_code']);
+                    $signature->setCity($data['city']);
+                    $signature->setEmail($data['email']);
+                    $signature->setPhone($data['phone']);
+                    $signature->setLogo($data['logo']);
+                    $signature->setUserId($session->get('user_id'));
+                    // Définir la date de création
+                    $createAt = new DateTimeImmutable();
+                    $signature->setCreateAt($createAt);
+                    // Définir la date de mise à jour (identique à la date de création initialement)
+                    $updateAt = clone $createAt;
+                    $signature->setUpdateAt($updateAt);
+                    // Enregistrer l'entité dans la base de données
+                    $entityManager->persist($signature);
+                    $entityManager->flush();
+                    // Générer la signature avec les données fournies
+                    $generatedSignature = $this->generateEmailSignature($data);
+                }
+            }
+        }
+        return $this->render('admin/create_signature.html.twig', [
+            'form' => $form->createView(),
+            'signature' => $generatedSignature,
+        ]);
+    }
+
+    private function generateEmailSignature(array $data): string
+    {
+        $html = '<!DOCTYPE html>';
+        $html .= '<html lang="fr">';
+        $html .= '<head>';
+        $html .= '<meta charset="utf-8">';
+        $html .= '<title>UNSA Signature</title>';
+        $html .= '</head>';
+        $html .= '<body>';
+        $html .= '<table border="0" cellpadding="0" width="500">';
+        $html .= '<tbody>';
+        $html .= '<tr>';
+        $html .= '<td align="left" valign="middle" width="10">';
+        $html .= '<p style="padding-inline-end: 10px;font-size: 12px;line-height: 14px;">';
+        $html .= '<a href="https://www.unsa.org"><img src="' . $data['logo']->getPath() . '" style="border: none;inline-size: 120px;"></a>';
+        $html .= '</p>';
+        $html .= '</td>';
+        $html .= '<td>';
+        $html .= '<p style="font-family: -apple-system, BlinkMacSystemFont, \'Segoe UI\', Roboto, \'Helvetica Neue\', Arial, sans-serif;font-size: 12px; line-height: 14px; color: #000;text-align: start;">';
+        $html .= '<span style="color: #000;font-weight: bold;font-size: 14px;">' . $data['name'] . '</span><br>';
+        $html .= '<span style="color: #666;"><i>' . $data['role'] . '</i></span><br>';
+        $html .= '<span style="color: #666;"><i>' . $data['organization'] . '</i></span><br>';
+        $html .= '</p>';
+        $html .= '<p style="font-family: -apple-system, BlinkMacSystemFont, \'Segoe UI\', Roboto, \'Helvetica Neue\', Arial, sans-serif;font-size: 12px; line-height: 14px; color: #000;">';
+        $html .= '<span style="color: #000;">' . $data['adress'] . '</span><br>';
+        $html .= '<span style="color: #000;">' . $data['zip_code'] . '&nbsp;</span>';
+        $html .= '<span style="color: #000;">' . $data['city'] . '</span><br>';
+        $html .= '</p>';
+        $html .= '<p style="font-family: -apple-system, BlinkMacSystemFont, \'Segoe UI\', Roboto, \'Helvetica Neue\', Arial, sans-serif;font-size: 12px; line-height: 14px; color: #000;">';
+        $html .= '<img src="https://reseaux.unsa.org/signature/_mail.svg" style="border: none;block-size: 12px;margin-inline-end: .5em;">';
+        $html .= '<a href="mailto:' . $data['email'] . '" style="color: #666;font-style: italic;">' . $data['email'] . '</a><br>';
+        $html .= '<img src="https://reseaux.unsa.org/signature/_phone.svg" style="border: none;block-size: 14px;margin-inline-end: .5em;">';
+        $html .= '<span style="color: #666;">' . $data['phone'] . '</span>';
+        $html .= '</p>';
+        $html .= '</td>';
+        $html .= '</tr>';
+        $html .= '</tbody>';
+        $html .= '</table>';
+        $html .= '</body>';
+        $html .= '</html>';
+
+        return $html;
     }
 }
